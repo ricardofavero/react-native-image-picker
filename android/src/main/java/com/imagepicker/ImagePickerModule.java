4b7ae9897b9d8a5a51c1b1cb809121ec0e43bb08
ricardofavero <gf.ricardo@hotmail.com>
2017-05-12 14:18:15 -0300

    fix bitmap

--- android/src/main/java/com/imagepicker/ImagePickerModule.java
+++ android/src/main/java/com/imagepicker/ImagePickerModule.java
@@ -6,6 +6,7 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
